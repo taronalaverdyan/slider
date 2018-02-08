c8f372cad43e302e47046a633378660ec3af4efc
Taron <taralav@mail.ru>
2018-02-08 20:16:04 +0400

    Color slider

---
+++ scripts.js
@@ -0,0 +1,27 @@
+$(function() {
+  $("#slider").rangeRover({
+    data: {
+      start: 1,
+      end: 100,
+      color: 'linear-gradient(to right, #ffffff, #6302ff);'
+    }
+  });
+});
+
+
+let users = [
+  {
+    user: "Bob",
+    salary: 10;
+    bonus : 5
+  }
+  {
+    user: "Tom",
+    salary: 15;
+    bonus : 2
+  }
+  {
+    user: "Jemma",
+    salary: 10000;
+  }
+]
